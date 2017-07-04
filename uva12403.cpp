079d4366af420c5021d1f02f9f90806753457054
teelau <tommylau94@gmail.com>
2017-07-04 10:55:36 -0700

    Update uva12403.cpp

--- uva12403.cpp
+++ uva12403.cpp
@@ -9,15 +9,17 @@
 using namespace std;
 
 int main() {
+	int n,m;
 	string s;
-	int n = 1;
-	while (cin >> s, s[0] != '*') {
-		cout << "Case " << n << ": ";
-		if (s == "Hajj")
-			cout << "Hajj-e-Akbar";
-		else
-			cout << "Hajj-e-Asghar";
-		cout << endl;
-		n++;
+	int sum = 0;
+	cin >> n;
+	while (n--) {
+		cin >> s;
+		if (s == "report")
+			cout << sum << endl;
+		else {
+			cin >> m;
+			sum += m;
+		}
 	}
 }
